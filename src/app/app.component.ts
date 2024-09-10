9d10a2ad2d15d6e07a0639c0a48ad1b4e8efc198
gotoharjeet <gotoharjeet@gmail.com>
2024-09-10 17:58:23 +0530

    angular:data binding

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { AddEmployeeComponent } from "./components/add-employee/add-employee.component";
+import { EmployeeListComponent } from "./components/employee-list/employee-list.component";
+import { DatabindingComponent } from "./components/databinding/databinding.component";
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, AddEmployeeComponent, EmployeeListComponent, DatabindingComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'angular-18';
+}
---
+++ src/app/components/databinding/databinding.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-databinding',
+  standalone: true,
+  imports: [],
+  templateUrl: './databinding.component.html',
+  styleUrl: './databinding.component.css'
+})
+export class DatabindingComponent {
+
+  courseName:string="Angular18";
+  inputType="checkbox";
+  rollNo:number=123;
+
+  isIndian:boolean=false;
+  currentDate:Date=new Date();
+  constructor()
+  {
+    
+  }
+}
