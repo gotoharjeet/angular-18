034e0f31950c13c52057d5397181dda3a0f2e8fe
gotoharjeet <gotoharjeet@gmail.com>
2024-09-14 16:49:44 +0530

    angular18: directives

--- src/app/components/directives/structural-directive/structural-directive.component.ts
+++ src/app/components/directives/structural-directive/structural-directive.component.ts
@@ -1,12 +1,42 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-structural-directive',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,FormsModule],
   templateUrl: './structural-directive.component.html',
   styleUrl: './structural-directive.component.css'
 })
 export class StructuralDirectiveComponent {
+
+  isDiv1Visible:boolean=true;
+  idDiv1Visible:boolean=true;
+  isSameDiv3Value:boolean=false;
+  number1:string="";
+  number2:string="";
+
+  
+  showDiv1(value:boolean):void
+  {
+    this.isDiv1Visible=value;
+  }
+
+  toggleDiv1()
+  {
+    this.idDiv1Visible=!this.idDiv1Visible;
+
+    // if(this.idDiv1Visible == false)
+    // {
+    //   this.idDiv1Visible=false;
+    // }
+    // else
+    // {
+    //   this.idDiv1Visible=true;
+    // }
+  }
+
+  
 
 }
